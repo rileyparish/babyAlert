00fab5a613792e843610a78e003ce280df6dbbfe
riley <parish.riley@gmail.com>
2021-02-10 15:37:23 -0700

    I can now send values from the front-end to the local server and write the data to a file.

--- updateBlinkRate.php
+++ updateBlinkRate.php
@@ -1,12 +1,22 @@
 <?php
 
-    // $data = $_POST["fname"];
+    // display php errors that occur:
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
+    // $data = $_POST["value"];
+    //get the data posted to this file
+    $json = file_get_contents('php://input');
+    // Convert it into a PHP object
+    $data = json_decode($json);
 
     $testFile = fopen("tempFile.txt", "a") or die("Unable to open file!");
     $entry = "Received post request";
-    fwrite($testFile, $entry);
+    fwrite($testFile, strval($data->{"value"}));
     fclose($testFile);
     // echo "Success";
-    echo 'Hello ' . $_POST["fname"] . '!';
+    echo 'Hello ' . strval($data->{"value"}) . '!';
     
 ?>
